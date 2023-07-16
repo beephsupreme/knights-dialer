48cc5532d573960c9ea79d0fc93759f37176e8b8
Michael Rowsey <mike@rowsey.org>
2023-07-16 15:12:54 -0700

    soln 2

--- js/dialer.js
+++ js/dialer.js
@@ -20,27 +20,45 @@ var nearbyKeys = [
 	[2, 4]
 ];
 
+
 function reachableKeys(startingDigit) {
 	return nearbyKeys[startingDigit];
 }
 
 function countPaths(startingDigit,hopCount) {
-	// TODO: given the digit/key to start from and
-	// the number of hops to take, return a count
-	// of all the possible paths that could be
-	// traversed
-	return 0;
+	if (hopCount == 0) return 1;
+	var pathCount = 0;
+	for (let digit of nearbyKeys[startingDigit]) {
+		pathCount += countPaths(digit, hopCount - 1);
+	}
+	return pathCount;
 }
 
 function listAcyclicPaths(startingDigit) {
-	// TODO: given the digit/key to start from,
-	// return a list of the distinct acyclic
-	// paths that are possible to traverse
-	//
-	// e.g. [
-	//   [4, 3, 8, 1, 6, 7, 2, 9],
-	//   [4, 3, 8, 1, 6, 0],
-	//   ...
-	// ]
-	return [];
+	var paths = [];
+	var nextHops = nearbyKeys[startingDigit];
+	for (let nextHop of nextHops) {
+		let path = [startingDigit, nextHop];
+		followPath(path, paths);
+	}
+	return paths; 
+}
+
+function followPath(path, paths) {
+	var nextHops = nearbyKeys[path[path.length - 1]];
+	var pathForwardFound = false;
+	
+	for (let nextHop of nextHops) {
+		if (!path.includes(nextHop)) {
+			pathForwardFound = true;
+			let nextPath = [ ...path, nextHop ];
+			followPath(nextPath, paths);
+		}
+	}
+		
+
+	if (!pathForwardFound) {
+		paths.push(path);
+	}
+
 }
